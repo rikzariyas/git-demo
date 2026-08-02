2ded9a03ec07386fd7e56f8fabee2fafd499e2f0
rikzariyas <rikza.dev@gmail.com>
2026-08-02 20:54:02 +0530

    remove unnecessary lines from test.js

--- test.js
+++ test.js
@@ -1,19 +1,1 @@
 console.log("hello world");
-gkjncnxzxncxcnskcnnzjcnc 
-xnjcbnxmnnxclc
-xnnxmcnxmcnjcslxmcxcm
-xncnjhcdklsx, ncfjv
-nxdjchdjcskxm n
-jsdjcndcndvvk
-dkmckdjidjfmc
-ncdjfjikcmc
-cdnfijmcld
-xsjncdokplxs,s.;c
-skxmojcdovb;nxc
-kdjcidjkcml;csmc
-cndikceidsaxmdifj
-sxishdwimzl<zx
-bdedendeofjm
-dneciojdsxlpd
-knxidheocmslx,
-nzxsijdwixm
