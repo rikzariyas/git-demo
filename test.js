bfe15edcd8defcc5024af4ca2de97906de01f1dc
rikzariyas <rikza.dev@gmail.com>
2026-08-02 21:22:36 +0530

    remove extraneous characters from test.js

--- test.js
+++ test.js
@@ -1,15 +1,1 @@
 console.log("hello world");
-gkjncnxzxncxcnskcnnzjcnc 
-xnjcbnxmnnxclc
-xnnxmcnxmcnjcslxmcxcm
-xncnjhcdklsx, ncfjv
-nxdjchdjcskxm n
-jsdjcndcndvvk
-dkmckdjidjfmc
-ncdjfjikcmc
-cdnfijmcld
-xsjncdokplxs,s.;c
-skxmojcdovb;nxc
-kdjcidjkcml;csmc
-cndikceidsaxmdifj
-sxishdwimzl<zx
