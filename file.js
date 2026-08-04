ab6204560b08acfcf15911b7352eda2da248e212
rikzariyas <rikza.dev@gmail.com>
2026-08-04 16:02:39 +0530

    fix:resolve merge conflicts and clean up redundant code

--- file.js
+++ file.js
@@ -1,24 +1,2 @@
 console.log("hello!");
-<<<<<<< HEAD
 alert("Hello World!!!");
-async function name(hello) {
-    console.log("welcome")
-}
-console.log("hello1!");
-alert("Hello World!");
-async function name(helloo) {
-    console.log("wellcome")
-}
-console.log("hello2!");
-alert("Hello World!!");
-async function name(heloo) {
-    console.log("welcomee")
-}
-console.log("hello3!");
-alert("Hello World!!!");
-async function name(hello) {
-    console.log("welcome!")
-}
-=======
-alert("Hello World!!!");
->>>>>>> 143e9df55ca2ac73018cd058d334f2c870b02e8b
